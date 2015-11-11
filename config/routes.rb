5a6d5ef429db00c0ba13e2c4967a052a0daff356
Art Beckman <aabeckman@yahoo.com>
2015-11-11 00:00:34 +0000

    Part way through chap 4, with mods for Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+Myrottenpotatoes::Application.routes.draw do
+  root 'movies#index'
+  resources :movies
+end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -53,4 +55,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+# end
