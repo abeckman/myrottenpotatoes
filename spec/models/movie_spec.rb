4a7b70d1f2164abe39adfa488b23675a15019b80
Art Beckman <aabeckman@yahoo.com>
2015-12-02 19:03:39 +0000

    added files missed the last time

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Movie do
+  describe 'searching Tmdb by keyword' do
+    context 'with valid API key' do
+      it 'should call Tmdb with title keywords' do
+        expect(Tmdb::Movie).to receive(:find).with('Inception')
+        Movie.find_in_tmdb('Inception')
+      end
+    end
+    context 'with invalid API' do
+      before :each do
+        allow(Tmdb::Movie).to receive(:find).and_raise(NoMethodError)
+        allow(Tmdb::Api).to receive(:response).and_return({'code' => 401})
+      end  
+      it 'should raise an InvalidKeyError with no API key' do
+        expect { Movie.find_in_tmdb('Inception') }.to raise_error(Movie::InvalidKeyError)
+      end
+    end
+  end
+end
